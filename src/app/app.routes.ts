016193150b234baa29384b9c31852918e3c55b51
MatyasLesznyak <matyas.lesznyak@voodoopark.com>
2025-06-16 22:10:35 +0200

    home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ClientListComponent } from './clients/client-list/client-list.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   {
     path: 'clients',
     component: ClientListComponent,
   },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
