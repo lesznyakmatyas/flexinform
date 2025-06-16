f7b74f9904c2e35fa73b4425a750883e53206c2d
MatyasLesznyak <matyas.lesznyak@voodoopark.com>
2025-06-16 17:13:33 +0200

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ClientsComponent } from './clients/clients.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, ClientsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'flexinform';
