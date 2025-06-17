bd40a107026f9eebc910b6abcc22a830ee399514
MatyasLesznyak <matyas.lesznyak@voodoopark.com>
2025-06-17 18:18:21 +0200

    exclude playwright tests from running by jest

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,7 @@
 export const preset = "jest-preset-angular";
 export const setupFilesAfterEnv = ["<rootDir>/setup-jest.ts"];
+export const testPathIgnorePatterns = [
+  "<rootDir>/node_modules/",
+  "<rootDir>/dist/",
+  "/tests/",
+];
