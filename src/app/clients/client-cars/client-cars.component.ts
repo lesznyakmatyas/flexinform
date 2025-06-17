333fc0e386a767854cb3d79e4e9113a64810c92c
MatyasLesznyak <matyas.lesznyak@voodoopark.com>
2025-06-17 13:39:39 +0200

    improve boolean value display

--- src/app/clients/client-cars/client-cars.component.ts
+++ src/app/clients/client-cars/client-cars.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { ClientCar } from '../../interfaces/clientDTOs';
 import { ClientsService } from '../../services/clients.service';
 import { CarServicesComponent } from '../../car-services/car-services.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-client-cars',
-  imports: [CarServicesComponent],
+  imports: [CarServicesComponent, NgStyle],
   templateUrl: './client-cars.component.html',
   styleUrl: './client-cars.component.css',
 })
